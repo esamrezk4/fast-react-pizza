48ded8576c6cc24e90e72cfd767676fbb7331f19
esamrezk4 <92589088+esamrezk4@users.noreply.github.com>
2023-08-10 06:15:55 +0300

    dispatch action to update username in all app

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,7 +1,7 @@
-import { Fragment, useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from './Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -44,6 +44,7 @@ function CreateOrder() {
 
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
+  const username = useSelector((state) => state.user.username);
 
   return (
     <div className="px-4 py-6">
@@ -53,7 +54,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow " type="text" name="customer" required />
+          <input
+            className="input grow "
+            defaultValue={username}
+            type="text"
+            name="customer"
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,9 @@
+import { useSelector } from 'react-redux';
 import CreateUser from '../features/user/CreateUser';
+import Button from '../features/order/Button';
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
   return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="mb-8  text-xl font-semibold md:text-3xl">
@@ -11,7 +14,13 @@ function Home() {
         </spna>
       </h1>
 
-      <CreateUser />
+      {username === '' ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          Continue Ordering , {username}
+        </Button>
+      )}
     </div>
   );
 }
